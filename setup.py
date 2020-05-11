2527dc66d89fac037996b55c08165558a3c2ac8a
André van Delft <andrevandelft@outlook.com>
2020-05-11 12:42:42 +0200

    Bump version number for new release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name = 'dh-utils',
-    version = '0.1.4',
+    version = '0.1.5',
     description = 'Python package containing various utilities relevant in the field of digital humanities.',
     packages = find_packages(),
     install_requires = ['regex', 'lxml'],
