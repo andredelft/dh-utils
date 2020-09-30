dcdc17847a854925623fc9429c9030674cb0145e
André van Delft <andrevandelft@outlook.com>
2020-09-30 12:52:20 +0200

    Directional markers are a headache

--- dh_utils/tei/tag_script.py
+++ dh_utils/tei/tag_script.py
@@ -29,7 +29,7 @@ COMBINING_PUNCT = {
 DIRECTION_MARKERS = '\u200e\u202a-\u202e'
 
 RE_STR = {
-    script: fr'[{RANGE[script]}{DIRECTION_MARKERS}][\s{RANGE[script]}{COMBINING_PUNCT[script]}]*(?<!\s)'
+    script: fr'[{DIRECTION_MARKERS}]*{RANGE[script]}][\s{RANGE[script]}{COMBINING_PUNCT[script]}{DIRECTION_MARKERS}]*(?<!\s)'
     for script in RANGE.keys()
 }
 
